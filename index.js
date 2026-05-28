8b088b3d065282475036bb87c4df7217cad987f2
JaeHyun Lim <acertainromance401@gmail.com>
2026-05-28 14:05:58 +0900

    ci: add npm publish, docker push, and security automation

---
+++ index.js
@@ -0,0 +1,10 @@
+function getPackageInfo() {
+  return {
+    name: "@acertainromance401/sentivision-utils",
+    purpose: "Utility metadata for SentiVision package workflows"
+  };
+}
+
+module.exports = {
+  getPackageInfo
+};
